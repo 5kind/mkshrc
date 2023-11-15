902bf43e02762dc1fab5d1618df8e9cfc5426bc5
5kind <weirui5671@outlook.com>
2023-11-15 10:44:26 +0800

    root-ext-bin

--- system/etc/profile.d/export-home.sh
+++ system/etc/profile.d/export-home.sh
@@ -1,21 +1,22 @@
 export_home(){
-    local neohome=$1
-    if [ -d "$neohome" ] ;then
-        export HOME=$neohome
+    local newhome=$1
+    if [ -d "$newhome" ] && [ -x "$newhome" ] ;then
+        export HOME=$newhome
     else
         export HOME=/
-	return 1
+	    return 1
     fi
 }
 
-# export root/shell home
+# export root/shell HOME
+# you can mklink /data/local/root -> /data/data/com.termux/files/home/.suroot
 if [ "$USER_ID" = 0 ] ;then
     export_home /data/local/root
 elif [ "$USER_ID" = 2000 ] ;then
     export_home /data/user_de/0/com.android.shell/files
 else
+    export_home /data/data/com.termux/files/home ||
     export HOME=/
 fi
-# elif; export_home /data/data/com.termux/files/home ;then ...
-# # export termux home is disable by default.
+
 unset -f export_home
---
+++ system/etc/profile.d/root-ext-bin.sh
@@ -0,0 +1,4 @@
+append_exist_path /data/adb/magisk
+append_exist_path /data/adb/ksu/bin
+command -v magisk64 >/dev/null 2>&1 &&
+alias magisk=magisk64
