6c42d2229192680f5d6af6adaa74a8e4e654f23e
5kind <shikabane320@gmail.com>
2026-09-13 16:00:59 +0800

    fix(path): update root bin search path to match /data/*/*/bin

--- system/etc/profile.d/append-root-bin.sh
+++ system/etc/profile.d/append-root-bin.sh
@@ -3,6 +3,6 @@ for system_bin in /*/*bin; do
 done
 
 for root_bin in $(ls -d /data/adb/magisk \
-    /data/adb/*/bin 2>/dev/null); do
+    /data/*/*/bin 2>/dev/null); do
     append_path $root_bin
 done
